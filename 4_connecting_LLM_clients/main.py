a6b56d57209fafa9b524ed16c42f74148e86cccc
Sagarnil Das <sagarnildass@gmail.com>
2025-04-23 09:21:23 +0530

    implemented session initialization and integrated HumanMessage for agent invocation in the MCP client

--- 4_connecting_LLM_clients/main.py
+++ 4_connecting_LLM_clients/main.py
@@ -6,6 +6,7 @@ from mcp.client.stdio import stdio_client
 from langchain_openai import ChatOpenAI
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
 
 load_dotenv(find_dotenv())
 
@@ -19,7 +20,15 @@ stdio_server_params = StdioServerParameters(
 )
 
 async def main():
-    print("Hello, world!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized")
+            tools = await load_mcp_tools(session)
+
+            agent = create_react_agent(llm, tools)
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3 ?")]})
+            print(result["messages"][-1].content)
     
 if __name__ == "__main__":
     asyncio.run(main())
